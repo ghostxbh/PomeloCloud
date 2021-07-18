fe7f512e9585bee1e208ebc93123af7b2835daf5
ghostxbh <ghostxbh@hotmail.com>
2021-07-18 15:03:38 +0800

    feat: 启动脚本、package.json、主配置

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * code by PomeloCloud
+ */
+const path = require('path');
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+// parse body
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// engine
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+// router
+app.use(require('./src/router/index'));
+
+module.exports = app;
---
+++ panel.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+/**
+ * ##############################################################################
+ * # |                              柚子云面板                                  | #
+ * ##############################################################################
+ * # |                                                                        | #
+ * # | Copyright (c) 2021-2099 柚子云(http://pomelo.work) All rights reserved. | #
+ * # |                                                                        | #
+ * ##############################################################################
+ * # |                           Team: uzykj.com                              | #
+ * ##############################################################################
+ */
+var debug = require('debug')('PomeloCloud:server');
+var http = require('http');
+var app = require('./app');
+var fs = require('fs');
+var PORT = fs.readFileSync('./config/port.pl');
+var port = normalizePort(process.env.PORT || PORT);
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+  console.log('Server is listening on port %f', port);
+}
