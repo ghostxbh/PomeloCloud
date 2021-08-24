9dad16946a87087da61d5263984281b615f386d7
ghostxbh <ghostxbh@hotmail.com>
2021-08-25 00:14:43 +0800

    fix(server): 增加公共配置下通用

--- pomelo-cloud-server/src/constant.js
+++ pomelo-cloud-server/src/constant.js
@@ -4,6 +4,13 @@
  */
 module.exports = Object.freeze(
   {
+    COMMON: {
+      SYSTEM_VERSION: {
+        LINUX: 'Linux',
+        MACOS: 'Darwin',
+        WINDOWS: 'Windows_NT',
+      }
+    },
     FILE_COMMON: {
       CONFIG_PATH: `${process.cwd()}/configuration/`,
       FILE: 'file.json',
