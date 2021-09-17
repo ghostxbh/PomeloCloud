bbe82306192e9618662c19f99cd780b362976faa
ghostxbh <ghostxbh@hotmail.com>
2021-09-17 10:16:44 +0800

    fix(server): 使用自定义响应

--- pomelo-cloud-server/src/router/api/file.router.js
+++ pomelo-cloud-server/src/router/api/file.router.js
@@ -5,16 +5,19 @@
 const express = require('express');
 const router = express.Router();
 const {FileService} = require('../../service/file.service');
+const logger = require('log4js').getLogger();
 
 router.post('/list', function (req, res, next) {
   const {path, options} = req.body.data;
   try {
     const fileList = FileService.fileList(path, options);
-    return res.json(fileList);
+    _ctx.setData(fileList);
+    _ctx.sendSuccess();
   } catch (e) {
-    console.error('file list: ', e);
-    next();
+    logger.error('[file] list: ', e);
+    _ctx.sendError(e);
   }
+  return res.json(_ctx.sendResult());
 });
 
 module.exports = router;
