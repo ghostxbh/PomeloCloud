104b220e16b5dc9884c1b91f016d2c979654f9ce
ghostxbh <ghostxbh@hotmail.com>
2021-09-18 15:36:05 +0800

    fix: 修改node安装脚本

--- environment/node_install.sh
+++ environment/node_install.sh
@@ -11,30 +11,6 @@ export PATH
 ##############################################################################
 # |                          author: ghostxbh                              | #
 ##############################################################################
-hasNetwork=0
-
-#检测网络链接畅通
-function network()
-{
-    #超时时间
-    local timeout=1
-
-    #目标网站
-    local target=www.baidu.com
-
-    #获取响应状态码
-    local ret_code=`curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1`
-
-    if [ "x$ret_code" = "x200" ]; then
-        #网络畅通
-        hasNetwork=1
-        return
-    else
-        #网络不畅通
-        hasNetwork=0
-        return
-    fi
-}
 
 nodeTarxz=node-v14.17.3-linux-x64.tar.xz
 nodeVersion=14.17.3
