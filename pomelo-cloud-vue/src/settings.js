81007f30186df6b0f5c324e27acf83adab06123a
ghostxbh <ghostxbh@hotmail.com>
2021-11-27 14:27:04 +0800

    feat: 增加基础框架

---
+++ pomelo-cloud-vue/src/settings.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+  /**
+   * @type {string}
+   * @description Title text
+   */
+  title: 'PomeloCloud',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show footer info
+   */
+  showFooter: true,
+
+  /**
+   * @type {string}
+   * @description Footer text
+   */
+  copyrights: '© 2020-2021 柚子云',
+
+  /**
+   * @type {string}
+   * @description Custom external links title
+   */
+  customLinkTitle: '寻求帮助 | 提问',
+
+  /**
+   * @type {string}
+   * @description Custom external links
+   */
+  customLink: 'https://gitee.com/ghostxbh/PomeloCloud/issues',
+};
